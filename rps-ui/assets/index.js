e0a2a96fa4e0f392daf51cf687bafa91fada51b9
Alex Rodriguez <alex20060420@gmail.com>
2023-12-13 09:01:54 -0600

    Add function for computer choice

---
+++ rps-ui/assets/index.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice == 0) {
+    choice = "rock";
+  } else if (choice == 1) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+  return choice;
+}
+
