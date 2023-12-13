a0049ade39177b8244a236b12fb0515b17719dfe
Alex Rodriguez <alex20060420@gmail.com>
2023-12-13 09:03:09 -0600

    Add function for rps logic

--- rps-ui/assets/index.js
+++ rps-ui/assets/index.js
@@ -10,3 +10,24 @@ function getComputerChoice() {
   return choice;
 }
 
+function playRound(playerSelection, computerSelection){
+  if (playerSelection == "rock" && computerSelection == "rock") {
+    return "It's a tie! Rock vs. Rock";
+  } else if (playerSelection == "paper" && computerSelection == "paper") {
+    return "It's a tie! Paper vs. Paper";
+  } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    return "It's a tie! Scissors vs. Scissors";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You won! Rock beats Scissors!";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You won! Paper beats Rock!";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You won! Scissors beats Paper!";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You lose! Paper beats Rock!";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "You lose! Scissors beats paper";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "You lose! Rock beats Scissors";
+  }
+}
