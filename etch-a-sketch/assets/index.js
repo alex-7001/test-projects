8da743aa610690d44efa10570a059006193d7f23
Alex Rodriguez <alex20060420@gmail.com>
2023-12-15 15:18:50 -0600

    Add 16x16 grid

---
+++ etch-a-sketch/assets/index.js
@@ -0,0 +1,12 @@
+const container = document.getElementsByClassName("container")[0];
+
+for (let i = 0; i < 256; i++) {
+  newBox = document.createElement("div");
+  container.appendChild(newBox);
+  newBox.setAttribute("style", "width: 20px; height: 20px;border: 1px solid;")
+}
+
+document.body.setAttribute("style", "padding: 0; margin: 0; box-sizing: border-box;")
+
+container.setAttribute("style", "display: flex; flex-wrap: wrap; width: 342px; margin: 60px auto 0px;");
+
