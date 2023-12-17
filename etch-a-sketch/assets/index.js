e78dcc12a4ff7e1f962d314df6c67419e456cb19
Alex Rodriguez <alex20060420@gmail.com>
2023-12-17 17:01:48 -0600

    Add semi colon at the end of code lines

--- etch-a-sketch/assets/index.js
+++ etch-a-sketch/assets/index.js
@@ -3,11 +3,12 @@ const container = document.getElementsByClassName("container")[0];
 for (let i = 0; i < 256; i++) {
   newBox = document.createElement("div");
   container.appendChild(newBox);
-  newBox.setAttribute("style", "width: 20px; height: 20px;border: 1px solid;")
-  newBox.setAttribute("class", "box")
+  newBox.setAttribute("style", "width: 20px; height: 20px;border: 1px solid;");
+  newBox.setAttribute("class", "box");
+  const Box = document.getElementsByClassName("box");
 }
 
-document.body.setAttribute("style", "padding: 0; margin: 0; box-sizing: border-box;")
+document.body.setAttribute("style", "padding: 0; margin: 0; box-sizing: border-box;");
 
 container.setAttribute("style", "display: flex; flex-wrap: wrap; width: 342px; margin: 60px auto 0px;");
 
