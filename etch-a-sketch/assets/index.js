1bb4886532998fc8a28cad557c76a5aa5f8f5a80
Alex Rodriguez <alex20060420@gmail.com>
2023-12-17 18:11:49 -0600

    Add hovering functionality on boxes

--- etch-a-sketch/assets/index.js
+++ etch-a-sketch/assets/index.js
@@ -1,14 +1,20 @@
 const container = document.getElementsByClassName("container")[0];
 
-for (let i = 0; i < 256; i++) {
-  newBox = document.createElement("div");
-  container.appendChild(newBox);
-  newBox.setAttribute("style", "width: 20px; height: 20px;border: 1px solid;");
-  newBox.setAttribute("class", "box");
-  const Box = document.getElementsByClassName("box");
-}
-
 document.body.setAttribute("style", "padding: 0; margin: 0; box-sizing: border-box;");
 
 container.setAttribute("style", "display: flex; flex-wrap: wrap; width: 342px; margin: 60px auto 0px;");
 
+for (let i = 0; i < 256; i++) {
+  const newBox = document.createElement("div");
+  container.appendChild(newBox);
+  newBox.setAttribute("style", "width: 20px; height: 20px;border: 1px solid;");
+  newBox.setAttribute("class", "box");
+}
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.addEventListener("mouseenter", () => {
+    box.style.backgroundColor = "blue";
+  });
+});
