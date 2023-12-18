d810ed35b0a7180b881f0fe08dd6439771006dc9
Alex Rodriguez <alex20060420@gmail.com>
2023-12-18 17:56:40 -0600

    Make grid 32 by 32

--- etch-a-sketch/assets/index.js
+++ etch-a-sketch/assets/index.js
@@ -4,10 +4,10 @@ document.body.setAttribute("style", "padding: 0; margin: 0; box-sizing: border-b
 
 container.setAttribute("style", "display: flex; flex-wrap: wrap; width: 342px; margin: 60px auto 0px;");
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 1024; i++) {
   const newBox = document.createElement("div");
   container.appendChild(newBox);
-  newBox.setAttribute("style", "width: 20px; height: 20px;border: 1px solid;");
+  newBox.setAttribute("style", "width: 9.3px; height: 9.3px;border: 1px solid;");
   newBox.setAttribute("class", "box");
 }
 
